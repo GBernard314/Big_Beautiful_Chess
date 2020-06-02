001d9bf50ec1eb2bf113b7da2d2b2005da20d827
SIMON <meta-block@hotmail.fr>
2020-06-02 16:26:44 +0200

    init rebase

---
+++ src/main/java/com/bigbeautifulchess/domain/User.java
@@ -0,0 +1,86 @@
+package com.bigbeautifulchess.domain;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name="users")
+public class User implements UserDetails {
+	private static final long serialVersionUID = -2963008589618789228L;
+	
+	@Id @Column
+	@GeneratedValue(generator = "seqUser")
+	@SequenceGenerator(name = "seqUser", sequenceName = "seq_user")
+	private Long id;
+
+	@Column(length = 100)
+	private String username;
+	
+	@Column(length = 100)
+	private String password;
+
+	@ManyToMany(fetch=FetchType.EAGER)
+	private Collection<Authority> authorities;
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Authority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Collection<Authority> authorities) {
+		this.authorities = authorities;
+	}
+}
