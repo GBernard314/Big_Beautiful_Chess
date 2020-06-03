1692525bb10f586cf8734be1ded9a5ea0731ec4a
TibRib <meta-block@hotmail.fr>
2020-06-03 15:14:08 +0200

    Removed javadoc

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.bigbeautifulchess.demo"},{"l":"com.bigbeautifulchess.engine"},{"l":"com.bigbeautifulchess.tools"}]
